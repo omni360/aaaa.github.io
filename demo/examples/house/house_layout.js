de3781215ec7c970855434818808bddda97b82d9
omni360 <omni360@qq.com>
2015-03-13 10:23:56 +0800

    添加家具菜单

---
+++ demo/examples/house/house_layout.js
@@ -0,0 +1,155 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "house_bak.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 4482,
+        "faces"         : 8556,
+        "normals"       : 2799,
+        "uvs"           : 0,
+        "materials"     : 11
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "ground_1",
+	"colorAmbient" : [0.14901961, 0.14901961, 0.14901961],
+	"colorDiffuse" : [0.14901961, 0.14901961, 0.14901961],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "wall_1_2",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "flltgrey",
+	"colorAmbient" : [0.5224, 0.5224, 0.5224],
+	"colorDiffuse" : [0.8245, 0.8245, 0.8245],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"specularCoef" : 1.0,
+	"transparency" : 0.5,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "white",
+	"colorAmbient" : [0.4, 0.4, 0.4],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.3, 0.3, 0.3],
+	"illumination" : 2,
+	"specularCoef" : 60.0
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "PAINT_gloss_black",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "METAL_brass_polish",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.64, 0.4],
+	"colorSpecular" : [0.5, 0.3, 0.3],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "METAL_aluminum",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.56, 0.56, 0.56],
+	"colorSpecular" : [0.705217, 0.705217, 0.705217],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 105.882355
+	},
+
+	{
+	"DbgColor" : 1458591,
+	"DbgIndex" : 7,
+	"DbgName" : "GLASS",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.72, 0.84, 0.84],
+	"colorSpecular" : [0.9, 0.9, 0.9],
+	"illumination" : 2,
+	"opticalDensity" : 1.021455,
+	"specularCoef" : 128.0,
+	"transparency" : 0.19999999,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 14968288,
+	"DbgIndex" : 8,
+	"DbgName" : "PAINT_gloss_white",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 11353132,
+	"DbgIndex" : 9,
+	"DbgName" : "METAL_brass_polis",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.64, 0.4],
+	"colorSpecular" : [0.5, 0.3, 0.3],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 12355935,
+	"DbgIndex" : 10,
+	"DbgName" : "197",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.64, 0.4],
+	"colorSpecular" : [0.5, 0.3, 0.3],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.07843
+	}],
+
+    "buffers": "house_layout.bin"
+
+}
