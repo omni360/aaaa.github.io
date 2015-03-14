e64fcf62f4dd6772ef9c45e9d6edd4cb7b251f15
omni360 <omni360@qq.com>
2015-03-14 15:00:14 +0800

    update

---
+++ demo/examples/house/userGuideExample.js
@@ -0,0 +1,312 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "userGuideExample.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 6207,
+        "faces"         : 10955,
+        "normals"       : 3242,
+        "uvs"           : 86,
+        "materials"     : 24
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "ground_1",
+	"colorAmbient" : [0.6627451, 0.69803923, 0.7176471],
+	"colorDiffuse" : [0.6627451, 0.69803923, 0.7176471],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "wall_1_2",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "wall_1_5",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"mapDiffuse" : "userGuideExample_wall_1_5.png",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "wall_5_29",
+	"colorAmbient" : [0.78039217, 0.8784314, 0.92941177],
+	"colorDiffuse" : [0.78039217, 0.8784314, 0.92941177],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "wall_6_32",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"mapDiffuse" : "userGuideExample_wall_6_32.png",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "white",
+	"colorAmbient" : [0.4, 0.4, 0.4],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.3, 0.3, 0.3],
+	"illumination" : 2,
+	"specularCoef" : 60.0
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "METAL_chrome",
+	"colorAmbient" : [0.8156863, 0.7490196, 0.5921569],
+	"colorDiffuse" : [0.8156863, 0.7490196, 0.5921569],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"specularCoef" : 128.0
+	},
+
+	{
+	"DbgColor" : 16059369,
+	"DbgIndex" : 7,
+	"DbgName" : "PLASTIC_shiny_white",
+	"colorAmbient" : [0.8156863, 0.7490196, 0.5921569],
+	"colorDiffuse" : [0.8156863, 0.7490196, 0.5921569],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"specularCoef" : 128.0
+	},
+
+	{
+	"DbgColor" : 1622631,
+	"DbgIndex" : 8,
+	"DbgName" : "cords",
+	"colorAmbient" : [0.8156863, 0.7490196, 0.5921569],
+	"colorDiffuse" : [0.8156863, 0.7490196, 0.5921569],
+	"colorSpecular" : [0.75061274, 0.75061274, 0.75061274],
+	"illumination" : 2,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 14218595,
+	"DbgIndex" : 9,
+	"DbgName" : "METAL_white_paint",
+	"colorAmbient" : [0.8156863, 0.7490196, 0.5921569],
+	"colorDiffuse" : [0.8156863, 0.7490196, 0.5921569],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"specularCoef" : 128.0
+	},
+
+	{
+	"DbgColor" : 5708456,
+	"DbgIndex" : 10,
+	"DbgName" : "fldkgrey",
+	"colorAmbient" : [0.0449, 0.0449, 0.0449],
+	"colorDiffuse" : [0.0939, 0.0939, 0.0939],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"specularCoef" : 1.0
+	},
+
+	{
+	"DbgColor" : 1556016,
+	"DbgIndex" : 11,
+	"DbgName" : "black",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.002, 0.002, 0.002],
+	"colorSpecular" : [0.5184, 0.5184, 0.5184],
+	"illumination" : 2,
+	"specularCoef" : 128.0
+	},
+
+	{
+	"DbgColor" : 12242867,
+	"DbgIndex" : 12,
+	"DbgName" : "sweethome3d_window_mirror_147",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"mapDiffuse" : "userGuideExample_sweethome3d_window_mirror_147.png",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 16326172,
+	"DbgIndex" : 13,
+	"DbgName" : "flltgrey",
+	"colorAmbient" : [0.5224, 0.5224, 0.5224],
+	"colorDiffuse" : [0.8245, 0.8245, 0.8245],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"specularCoef" : 1.0,
+	"transparency" : 0.5,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 5647119,
+	"DbgIndex" : 14,
+	"DbgName" : "yellowbrt",
+	"colorAmbient" : [0.4, 0.4, 0.4],
+	"colorDiffuse" : [1.0, 0.7837, 0.0],
+	"colorSpecular" : [0.3, 0.3, 0.3],
+	"illumination" : 2,
+	"specularCoef" : 60.0
+	},
+
+	{
+	"DbgColor" : 2608512,
+	"DbgIndex" : 15,
+	"DbgName" : "PAINT_gloss_white",
+	"colorAmbient" : [0.46666667, 0.21960784, 0.21960784],
+	"colorDiffuse" : [0.46666667, 0.21960784, 0.21960784],
+	"colorSpecular" : [0.75061274, 0.75061274, 0.75061274],
+	"illumination" : 2,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 10128841,
+	"DbgIndex" : 16,
+	"DbgName" : "METAL_brass_polish",
+	"colorAmbient" : [0.46666667, 0.21960784, 0.21960784],
+	"colorDiffuse" : [0.46666667, 0.21960784, 0.21960784],
+	"colorSpecular" : [0.75061274, 0.75061274, 0.75061274],
+	"illumination" : 2,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 8996413,
+	"DbgIndex" : 17,
+	"DbgName" : "METAL_aluminum",
+	"colorAmbient" : [0.46666667, 0.21960784, 0.21960784],
+	"colorDiffuse" : [0.46666667, 0.21960784, 0.21960784],
+	"colorSpecular" : [0.8272059, 0.8272059, 0.8272059],
+	"illumination" : 2,
+	"specularCoef" : 105.882355
+	},
+
+	{
+	"DbgColor" : 13579988,
+	"DbgIndex" : 18,
+	"DbgName" : "192",
+	"colorAmbient" : [0.46666667, 0.21960784, 0.21960784],
+	"colorDiffuse" : [0.46666667, 0.21960784, 0.21960784],
+	"colorSpecular" : [0.75061274, 0.75061274, 0.75061274],
+	"illumination" : 2,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 13541364,
+	"DbgIndex" : 19,
+	"DbgName" : "METAL_brass_polis",
+	"colorAmbient" : [0.46666667, 0.21960784, 0.21960784],
+	"colorDiffuse" : [0.46666667, 0.21960784, 0.21960784],
+	"colorSpecular" : [0.75061274, 0.75061274, 0.75061274],
+	"illumination" : 2,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 9886237,
+	"DbgIndex" : 20,
+	"DbgName" : "194",
+	"colorAmbient" : [0.46666667, 0.21960784, 0.21960784],
+	"colorDiffuse" : [0.46666667, 0.21960784, 0.21960784],
+	"colorSpecular" : [0.75061274, 0.75061274, 0.75061274],
+	"illumination" : 2,
+	"specularCoef" : 96.07843
+	},
+
+	{
+	"DbgColor" : 13519402,
+	"DbgIndex" : 21,
+	"DbgName" : "room_1_202",
+	"colorAmbient" : [0.47058824, 0.48235294, 0.5294118],
+	"colorDiffuse" : [0.47058824, 0.48235294, 0.5294118],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 11712834,
+	"DbgIndex" : 22,
+	"DbgName" : "room_1_203",
+	"colorAmbient" : [0.99607843, 0.99607843, 1.0],
+	"colorDiffuse" : [0.99607843, 0.99607843, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 109031,
+	"DbgIndex" : 23,
+	"DbgName" : "room_2_204",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"mapDiffuse" : "userGuideExample_room_2_204.png",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	}],
+
+    "buffers": "userGuideExample.bin"
+
+}
