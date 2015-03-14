6604cfe9d288908d0fb0a40d4a5882831dcc517d
omni360 <omni360@qq.com>
2015-03-14 16:19:16 +0800

    update

---
+++ demo/examples/custom/female02.js
@@ -0,0 +1,64 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "female02.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 3272,
+        "faces"         : 12466,
+        "normals"       : 6976,
+        "uvs"           : 3955,
+        "materials"     : 4
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "1",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.1, 0.1, 0.1],
+	"illumination" : 2,
+	"mapDiffuse" : "female02_1.jpg",
+	"specularCoef" : 0.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "2",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.1, 0.1, 0.1],
+	"illumination" : 2,
+	"mapDiffuse" : "female02_2.jpg",
+	"specularCoef" : 0.0
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "3",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.1, 0.1, 0.1],
+	"illumination" : 2,
+	"specularCoef" : 0.0
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "10",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.1, 0.1, 0.1],
+	"illumination" : 2,
+	"mapDiffuse" : "female02_10.jpg",
+	"specularCoef" : 0.0
+	}],
+
+    "buffers": "female02.bin"
+
+}
