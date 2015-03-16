a1af4288f12d0ae4f6920fd4d769932155fcc1dc
omni360 <omni360@qq.com>
2015-03-16 19:12:14 +0800

    add calculateVolumeOf3dMesh.js

--- demo/build/three.js
+++ demo/build/three.js
@@ -9435,6 +9435,39 @@ THREE.Geometry.prototype = {
 
 	},
 
+
+	calculateVolumeOfGeometry: function	(){
+		var vols = 0;
+
+		function SingedVolumeOfTriangle(p1, p2, p3) {
+			var v321 = p3.x * p2.y * p1.z;
+			var v231 = p2.x * p3.y * p1.z;
+			var v312 = p3.x * p1.y * p2.z;
+			var v132 = p1.x * p3.y * p2.z;
+			var v213 = p2.x * p1.y * p3.z;
+			var v123 = p1.x * p2.y * p3.z;
+
+			return (1.0 / 6.0) * (-v321 + v231 + v312 - v132 - v213 + v123);
+
+		}
+
+		for(i = 0; i < this.faces.length; i++) {
+
+			var a = this.faces[i].a;
+			var b = this.faces[i].b;
+			var c = this.faces[i].c;
+
+			var p1 = this.vertices[a];
+			var p2 = this.vertices[b];
+			var p3 = this.vertices[c];
+
+			vols += SingedVolumeOfTriangle(p1, p2, p3);
+
+		}
+
+		return vols;
+	},
+
 	fromBufferGeometry: function ( geometry ) {
 
 		var scope = this;
