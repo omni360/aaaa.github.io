9271d1c5d4fcbc23be0818f90e97aa9bb8cf18ff
omni360 <omni360@qq.com>
2015-04-12 18:08:36 +0800

    update

---
+++ demo/examples/custom/female11.js
@@ -0,0 +1,115 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "female11.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 0,
+        "faces"         : 40688,
+        "normals"       : 21977,
+        "uvs"           : 23037,
+        "materials"     : 7
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "material_0",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 0.94902, 0.847059],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"mapDiffuse" : "young_lightskinned_female_diffuse3.png",
+	"specularCoef" : 0.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "material_1",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"mapDiffuse" : "ponytail01_diffuse.png",
+	"specularCoef" : 0.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "material_2",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"mapDiffuse" : "brown_eye.png",
+	"specularCoef" : 0.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "material_3",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"mapDiffuse" : "eyebrow008.png",
+	"specularCoef" : 0.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "material_4",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"mapDiffuse" : "eyelashes02.png",
+	"specularCoef" : 0.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "material_5",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [0.639216, 0.639216, 0.639216],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"mapDiffuse" : "teeth.png",
+	"specularCoef" : 0.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "material_6",
+	"colorAmbient" : [0.2, 0.2, 0.2],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"mapDiffuse" : "female_casualsuit01_diffuse.png",
+	"specularCoef" : 0.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	}],
+
+    "buffers": "female11.bin"
+
+}
