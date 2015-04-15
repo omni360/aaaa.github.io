bfa5d810a83ca43eb724b150ce10e1df2c95f0a5
omni360 <omni360@qq.com>
2015-04-15 12:44:36 +0800

    update

---
+++ demo/examples/custom/female1.js
@@ -0,0 +1,16 @@
+{
+    "metadata" : {
+        "type" : "Object",
+        "version" : 4.3,
+        "generator" : "Blender Script"
+    },
+    "object" : {
+        "name" : "female1",
+        "type" : "Object3D",
+        "uuid" : "06313b69-3d58-5f3d-9193-89236bca42e4",
+        "matrix" : [1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1],
+        "children" : []
+    },
+    "geometries" : [],
+    "materials" : []
+}
